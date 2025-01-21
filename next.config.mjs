9515c298d55e1c46db888149952f77eb6465a9da
ST4LTH <66903577+ST4LTH@users.noreply.github.com>
2025-01-21 17:54:31 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-/*     basePath: "/portfolio",
+    basePath: "/portfolio",
     output: "export", 
-    reactStrictMode: true, */
+    reactStrictMode: true,
 };
 
 export default nextConfig;
